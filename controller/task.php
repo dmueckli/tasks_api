b2fdad063b0991bb4b202493688adb1213f7202a
muecklichd <muecklich.dennis@gmail.com>
2020-01-26 04:19:36 +0100

    Added function to query all tasks

--- controller/task.php
+++ controller/task.php
@@ -190,4 +190,73 @@ if (array_key_exists('taskid', $_GET)) {
         $response->send();
         exit();
     }
+} elseif (empty($_GET)) {
+
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+        try {
+
+            $query = $readDB->prepare('SELECT id, title, description, DATE_FORMAT(deadline, "%d/%m/%Y %H:%i") as deadline, completed FROM tbltasks');
+
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+
+            $taskArray = array();
+
+            while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+
+                $task = new Task($row['id'], $row['title'], $row['description'], $row['deadline'], $row['completed'],);
+
+                $taskArray[] = $task->returnTaskAsArray();
+            }
+
+            $returnData = array();
+            $returnData['rows_returned'] = $rowCount;
+            $returnData['tasks'] = $taskArray;
+
+            $response = new Response();
+            $response->setHttpStatusCode(200);
+            $response->setSuccess(true);
+            $response->toCache(true);
+            $response->setData($returnData);
+            $response->send();
+            exit();
+        } catch (TaskException $te) {
+
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage($te->getMessage());
+            $response->send();
+            exit();
+        } catch (PDOException $ex) {
+
+            error_log('Database query error - ' . $ex, 0);
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage('Failed to get tasks.');
+            $response->send();
+            exit();
+        }
+    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        # code...
+    } else {
+
+        $response = new Response();
+        $response->setHttpStatusCode(405);
+        $response->setSuccess(false);
+        $response->addMessage('Request method not allowed.');
+        $response->send();
+        exit();
+    }
+} else {
+
+    $response = new Response();
+    $response->setHttpStatusCode(404);
+    $response->setSuccess(false);
+    $response->addMessage('Endpoint not found.');
+    $response->send();
+    exit();
 }
